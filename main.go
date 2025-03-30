d69113681524741930f5aad0e8b2012eaa09f473
Deniska2006 <potorokad@gmail.com>
2025-03-30 19:47:23 +0300

    WorkOnlyDb(DeleteTxtFunctions)

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
-	"os"
 	"prj-test/config"
 	"prj-test/dbm"
 	"prj-test/domain"
@@ -15,41 +14,41 @@ import (
 	"github.com/upper/db/v4/adapter/postgresql"
 )
 
-func Id() int {
-	file, err := os.Open("finance.txt")
-	if err != nil {
-		log.Print("Помилка відкриття файлу:", err)
-		return 0
-	}
-	defer file.Close()
+// func Id() int {
+// 	file, err := os.Open("finance.txt")
+// 	if err != nil {
+// 		log.Print("Помилка відкриття файлу:", err)
+// 		return 0
+// 	}
+// 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	var lastLine string
+// 	scanner := bufio.NewScanner(file)
+// 	var lastLine string
 
-	for scanner.Scan() {
-		lastLine = scanner.Text()
-	}
+// 	for scanner.Scan() {
+// 		lastLine = scanner.Text()
+// 	}
 
-	if lastLine == "" {
-		return 1
-	}
+// 	if lastLine == "" {
+// 		return 1
+// 	}
 
-	idS := ""
-	for _, v := range lastLine {
-		if string(v) == " " || string(v) == "\t" {
-			break
-		}
-		idS += string(v)
-	}
-	id, err := strconv.Atoi(idS)
-	if err != nil {
-		log.Printf("Не коректний id у файлі: %v", err)
-		return -1
-	}
+// 	idS := ""
+// 	for _, v := range lastLine {
+// 		if string(v) == " " || string(v) == "\t" {
+// 			break
+// 		}
+// 		idS += string(v)
+// 	}
+// 	id, err := strconv.Atoi(idS)
+// 	if err != nil {
+// 		log.Printf("Не коректний id у файлі: %v", err)
+// 		return -1
+// 	}
 
-	id++
-	return id
-}
+// 	id++
+// 	return id
+// }
 
 func Price(data string) int {
 	price := -1
@@ -81,15 +80,27 @@ func Name(data string) string {
 }
 func InsertData(sess db.Session, data string) {
 
-	file, err := os.OpenFile("finance.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
-	if err != nil {
-		log.Print("Помилка відкриття файлу:", err)
-		return
+	// file, err := os.OpenFile("finance.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
+	// if err != nil {
+	// 	log.Print("Помилка відкриття файлу:", err)
+	// 	return
+	// }
+
+	// defer file.Close()
+
+	var lastID sql.NullInt64
+	row, _ := sess.SQL().QueryRow("SELECT MAX(id) FROM costs")
+	err := row.Scan(&lastID)
+
+	if err != nil && err != sql.ErrNoRows {
+		log.Fatal("Помилка виконання SQL:", err)
 	}
 
-	defer file.Close()
+	id := 1
+	if lastID.Valid {
+		id = int(lastID.Int64) + 1
+	}
 
-	id := Id()
 	name := Name(data)
 	price := Price(data)
 
@@ -107,11 +118,11 @@ func InsertData(sess db.Session, data string) {
 		log.Fatal("Error inserting data: ", err)
 	}
 
-	_, err = file.WriteString(strconv.Itoa(id) + "\t" + name + "\t" + strconv.Itoa(price) + "\t" + date.Format("2006-01-02 15:04:05") + "\n")
-	if err != nil {
-		log.Print("Помилка запису у файл:", err)
-		return
-	}
+	// _, err = file.WriteString(strconv.Itoa(id) + "\t" + name + "\t" + strconv.Itoa(price) + "\t" + date.Format("2006-01-02 15:04:05") + "\n")
+	// if err != nil {
+	// 	log.Print("Помилка запису у файл:", err)
+	// 	return
+	// }
 
 }
 
